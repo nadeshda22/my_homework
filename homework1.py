43161b7d8865c9c479cd37fc70a8013fad092f38
yanushevskayanadya <yanushevskaya@gmail.com>
2026-02-10 17:15:41 +0300

    editing README and requirements.txt
    add homework1.py and eror_pep8_and_pep20.py
    
    delete main.py
    
    homework1.py writing autotests for Google and GitHub
    
    eror_pep8_and_pep20.py bug fixes

---
+++ homework1.py
@@ -0,0 +1,54 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    opts = Options()
+
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
+
+
+@pytest.mark.true
+def test_selenium_web_google(driver):
+    url_google = "https://www.google.com/"
+
+    driver.get(url_google)
+
+    assert driver.title == "Google"
+    assert driver.current_url == url_google
+
+@pytest.mark.true
+def test_selenium_web_githab(driver):
+   url_githab = "https://github.com/"
+
+   driver.get(url_githab)
+
+   assert driver.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+   assert driver.current_url == url_githab
+
+
+@pytest.mark.false
+def test_selenium_web_google(driver):
+    url_google = "https://www.google.com/"
+
+    driver.get(url_google)
+
+    assert driver.title != "Google"
+    assert driver.current_url != url_google
+
+
+@pytest.mark.false
+def test_selenium_web_githab(driver):
+   url_githab = "https://github.com/"
+
+   driver.get(url_githab)
+
+   assert driver.title != "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+   assert driver.current_url != url_githab
